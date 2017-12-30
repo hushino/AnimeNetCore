173944d004e03d851f02d592ec20563f227f36d9
Hushino <frostshock@outlook.com>
2017-12-30 01:01:33 -0300

    ... nel

---
+++ AnimeNetCore/DAL/BlogRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using AnimeNetCore.Data;
+
+namespace AnimeNetCore.DAL
+{
+    public class BlogRepository : IBlogRepository, IDisposable
+    {
+        private BlogDbContext _context;
+
+        public BlogRepository(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+            {
+                Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+        }
+   
+}
---
+++ AnimeNetCore/DAL/IBlogRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeNetCore.DAL
+{
+    public interface IBlogRepository : IDisposable
+    {
+
+    }
+}
