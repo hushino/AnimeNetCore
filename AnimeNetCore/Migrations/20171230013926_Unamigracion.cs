af36a63375eee39af362b24f6e94c1aa2e8adbe4
Hushino <frostshock@outlook.com>
2017-12-29 22:43:28 -0300

    Pull wut?

---
+++ AnimeNetCore/Migrations/20171230013926_Unamigracion.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace AnimeNetCore.Migrations
+{
+    public partial class Unamigracion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
