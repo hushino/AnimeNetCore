7d91805c9eca456fbc3b5cb4f8ab87a131de89c7
Hushino <frostshock@outlook.com>
2017-12-29 21:03:24 -0300

    pull pull pull

--- AnimeNetCore/Data/IdentityModels.cs
+++ AnimeNetCore/Data/IdentityModels.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AnimeNetCore.Models;
+﻿using AnimeNetCore.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace AnimeNetCore.Data
